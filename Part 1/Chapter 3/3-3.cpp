a50099af4ead4061ac94caf26ee4500ba337463c
Gabriel <gabe.luculescu@gmail.com>
2020-10-01 18:11:12 -0500

    Improved both insert functions

--- Part 1/Chapter 3/3-3.cpp
+++ Part 1/Chapter 3/3-3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-/*  Add an Element* previous to Listing 3-9 to make a doubly linked list. Add
+/*
+    Add an Element* previous to Listing 3-9 to make a doubly linked list. Add
     an insert_before method to Element. Traverse the list from front to back, then
     from back to front, using two separate for loops. Print the operating_number
     inside each loop
@@ -7,26 +8,42 @@
 
 struct Element
 {
-    Element* next{};
-    Element* previous{};
+    Element* next;
+    Element* previous;
+
+    Element(const short on)
+        : operating_number{ on },
+        next{ nullptr },
+        previous{ nullptr }
+    { }
 
     void insert_after(Element* new_element)
     {
+        if (new_element == nullptr)
+            return;
+
         new_element->previous = this;
-        if(next)
-            next->previous = new_element;
 
-        new_element->next = next;
-        next = new_element;
+        if (this->next)
+            this->next->previous = new_element;
 
+        new_element->next = this->next;
+        this->next = new_element;
     }
 
     void insert_before(Element* new_element)
     {
-        new_element->previous = this;
-        if (next) next->previous = new_element;
-        new_element->next = next;
-        next = new_element;
+        if (new_element == nullptr)
+            return;
+
+        new_element->next = this;
+
+        if (this->previous)
+            this->previous->next = new_element;
+
+        new_element->previous = this->previous;
+        this->previous = new_element;
+
     }
 
     char prefix[2];
@@ -35,31 +52,26 @@ struct Element
 
 int main()
 {
-    Element trooper1, trooper2, trooper3, trooper4;
-
-    trooper1.operating_number = 421;
+    Element trooper1{ 421 },
+        trooper2{ 2187 },
+        trooper3{ 005 };
 
     trooper1.insert_after(&trooper2);
-
-    trooper2.operating_number = 2187;
-
     trooper2.insert_after(&trooper3);
 
-    trooper3.operating_number = 555;
-
-    trooper4.operating_number = 625;
-
+    Element trooper4{ 111 };
     trooper1.insert_before(&trooper4);
 
-    for (Element *cursor = &trooper1; cursor; cursor = cursor->next)
-    {
-        std::cout << cursor->operating_number << " --> ";
-    }
-    std::cout << "nullptr\n";
 
-    for(Element *cursor = &trooper3; cursor; cursor = cursor->previous)
+    for (Element* cursor = &trooper4; cursor; cursor = cursor->next)
     {
-        std::cout << cursor->operating_number << " --> ";
+        std::cout << cursor->operating_number << '\t';
     }
-    std::cout << "nullptr\n";
+    std::cout << '\n';
+
+    for (Element* cursor = &trooper3; cursor; cursor = cursor->previous)
+    {
+        std::cout << cursor->operating_number << '\t';
+    }
+    std::cout << '\n';
 }
