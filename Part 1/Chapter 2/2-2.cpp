27484d02b75fec07c568505bbb49581d1a4ad197
Gabriel <gabe.luculescu@gmail.com>
2020-10-01 12:51:32 -0500

    removed const prefix in constructor

--- Part 1/Chapter 2/2-2.cpp
+++ Part 1/Chapter 2/2-2.cpp
@@ -1,11 +1,21 @@
-/* Create a struct Calculator. It should have a single constructor that takes
+/* 
+   Create a struct Calculator. It should have a single constructor that takes
    an Operation.
 */
 
+enum class Operation
+{
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE
+};
+
+
 struct Calculator
 {
-    Calculator(const Operation operation)
-       : operation{operation} {}
+    Calculator(Operation operation)
+       : operation{operation} { }
 
 private:
     Operation operation;
