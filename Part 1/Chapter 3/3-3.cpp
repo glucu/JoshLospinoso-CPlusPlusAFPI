2b38570fe8c48c5ec8846f950c41bcf53ae6b291
Gabriel <gabe.luculescu@gmail.com>
2020-11-25 14:50:06 -0600

    Changed a lot including struct to class

--- Part 1/Chapter 3/3-3.cpp
+++ Part 1/Chapter 3/3-3.cpp
@@ -1,77 +1,130 @@
 #include <iostream>
 /*
-    Add an Element* previous to Listing 3-9 to make a doubly linked list. Add
-    an insert_before method to Element. Traverse the list from front to back, then
-    from back to front, using two separate for loops. Print the operating_number
-    inside each loop
-*/
+ *   Add an Element* previous to Listing 3-9 to make a doubly linked list. Add
+ *   an insert_before method to Element. Traverse the list from front to back, then
+ *   from back to front, using two separate for loops. Print the operating_number
+ *   inside each loop
+ */
 
-struct Element
+/* COMMENT
+ *
+ *  I know this isn't the way from the book, but
+ *  because we have defined both behaviors (functions)
+ *  we will like to encapsulate the data by seperating the two
+ *  with public interface, and implementation details. 
+ *  Also, as long as your member function contains 1-2 lines
+ *  of code, define them inline. Anything larger should be placed
+ *  outside with the scope resolution operator. 
+ */
+
+class Link
 {
-    Element* next;
-    Element* previous;
+public:
+    short operation_number;
 
-    Element(const short on)
-        : operating_number{ on },
-        next{ nullptr },
-        previous{ nullptr }
-    { }
+    Link(const short on);
+    Link* insert_after(Link* new_element);
+    Link* insert_before(Link* new_element);
 
-    void insert_after(Element* new_element)
+    void traverse_forward(Link* main_trooper) const;
+    void traverse_backwards(Link* main_trooper) const;
+
+    Link* get_next() const
+    { return next; }
+
+    Link* get_prev() const 
+    { return prev; }
+
+private:
+    Link *next;
+    Link *prev;
+};
+
+Link::Link(const short on)
+    : operation_number{on},
+      next{nullptr},
+      prev{nullptr} { }
+
+Link* Link::insert_after(Link* new_element)
+{
+    if (!new_element)  return this;
+    if (!this)         return new_element;
+
+    new_element->prev = this;
+    
+    if (this->next)
+        this->next->prev = new_element;
+
+    new_element->next = this->next;
+    this->next = new_element;
+
+    return new_element;
+}
+
+Link* Link::insert_before(Link* new_element)
     {
-        if (new_element == nullptr)
-            return;
-
-        new_element->previous = this;
-
-        if (this->next)
-            this->next->previous = new_element;
-
-        new_element->next = this->next;
-        this->next = new_element;
-    }
-
-    void insert_before(Element* new_element)
-    {
-        if (new_element == nullptr)
-            return;
+        if (!new_element)  return this;
+        if (!this)         return new_element;
 
         new_element->next = this;
 
-        if (this->previous)
-            this->previous->next = new_element;
+        if (this->prev)
+            this->prev->next = new_element;
 
-        new_element->previous = this->previous;
-        this->previous = new_element;
+        new_element->prev = this->prev;
+        this->prev = new_element;
 
+        return new_element;
     }
 
-    char prefix[2];
-    short operating_number;
-};
+void Link::traverse_forward(Link* main_trooper) const
+{
+    if(main_trooper) 
+    {
+        const Link* curr{main_trooper};
+        std::cout << "{ ";
+        while(curr) 
+        {
+            std::cout << curr->operation_number;
+            if(curr = curr->get_next())
+                std::cout << " <=> ";
+        }
+        std::cout << " }\n";
+    }
+}
+
+void Link::traverse_backwards(Link *main_trooper) const
+{
+    const Link* tail{main_trooper};
+
+    while(tail->get_next())
+        tail = tail->get_next();
+
+    std::cout << "{ ";
+    while(tail)
+    {
+        std::cout << tail->operation_number;
+        if(tail = tail->get_prev())
+            std::cout << " <=> ";
+    }
+    std::cout << " }\n";
+}
 
 int main()
 {
-    Element trooper1{ 421 },
-        trooper2{ 2187 },
-        trooper3{ 005 };
 
-    trooper1.insert_after(&trooper2);
-    trooper2.insert_after(&trooper3);
+    Link *main_trooper{new Link{2187}};
 
-    Element trooper4{ 111 };
-    trooper1.insert_before(&trooper4);
+    Link* trooper1{new Link{24}};
+    Link* trooper2{new Link{55}};
+    Link* trooper3{new Link{111}};
+    
+    main_trooper = main_trooper->insert_before(trooper1);
+    main_trooper = main_trooper->insert_before(trooper2);
+    main_trooper = main_trooper->insert_before(trooper3);
+    
+    main_trooper->traverse_forward(main_trooper);
+    main_trooper->traverse_backwards(main_trooper);
 
-
-    for (Element* cursor = &trooper4; cursor; cursor = cursor->next)
-    {
-        std::cout << cursor->operating_number << '\t';
-    }
-    std::cout << '\n';
-
-    for (Element* cursor = &trooper3; cursor; cursor = cursor->previous)
-    {
-        std::cout << cursor->operating_number << '\t';
-    }
-    std::cout << '\n';
+    return 0;
 }
