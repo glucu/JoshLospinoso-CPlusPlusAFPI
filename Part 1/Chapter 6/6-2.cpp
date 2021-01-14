d82ae6a149cdddc389cea15023fdab4cccd4ca18
Gabriel <gabe.luculescu@gmail.com>
2021-01-14 09:25:27 -0600

    Refactored error message in function mode and removed unneeded type trait header

--- Part 1/Chapter 6/6-2.cpp
+++ Part 1/Chapter 6/6-2.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
 #include <algorithm>
-#include <type_traits>
 
 // Implement mode as a template function
 
 template <typename T>
-T mode(const T *values, std::size_t length)
-{
+T mode(const T *values, std::size_t length) {
+
 	if (length <= 0) return 0;
-	if (!values)     throw std::invalid_argument{ "Invalid argument passed." };
+	if (!values)     throw std::invalid_argument{ "mode(const int *values, std::size_t length): null pointer" };
 
 	T number{ values[0] };
 	T mode{ number };
 	int count{ 1 };
 	int count_mode{ 1 };
 
-	for (std::size_t i{ 1 }; i < length; ++i)
-	{
+	for (std::size_t i{ 1 }; i < length; ++i) {
+
 		if (values[i] == number)
 			++count;
-		else
-		{
-			if (count > count_mode)
-			{
+		else {
+
+			if (count > count_mode) {
+
 				count_mode = count;
 				mode = number;
 			}
@@ -35,8 +34,8 @@ T mode(const T *values, std::size_t length)
 	return mode;
 }
 
-int main()
-{
+int main() {
+
 	int values[]{ 2, 8, 1, 1, 2, 9, 1, 5, 5, 3 };
 	std::size_t length{ std::size(values) };
 
@@ -48,7 +47,7 @@ int main()
 		// If exception is thrown, this won't execute.
 		std::cout << "mode: " << result << '\n';
 	}
-	catch (const std::exception &e) {
+	catch (const std::invalid_argument &e) {
 
 		std::cerr << "exception: " << e.what() << '\n';
 		return 1;
