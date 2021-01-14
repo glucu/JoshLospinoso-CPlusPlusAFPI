e1bcfedc5530f2b55b7204f5240606acfa22ab98
Gabriel <gabe.luculescu@gmail.com>
2021-01-14 09:51:45 -0600

    Refactored mode function and included missing header file

--- Part 1/Chapter 6/6-3.cpp
+++ Part 1/Chapter 6/6-3.cpp
@@ -2,40 +2,41 @@
 #include <algorithm>
 #include <concepts>
 #include <type_traits>
+#include <iterator>
 
 /*   Modify mode to accept an Integer concept. Verify that mode fails to
  *   instantiate with floating types like double.
  */
 
 /*  COMMENT
- *  Visual Studio 2019 supports concepts that you can find in the Standard Libaray headers.
+ *  Visual Studio 2019 19.23 (partial) supports concepts that you can find in the Standard Libaray headers.
  *  A floating-point type is not an integral. I also want to support other types such as
  *  short, long long, so I went with the std::is_integral_v<T> concept.
  *  Try reading: https://www.modernescpp.com/index.php/c-20-concepts-the-details
  */
 
 template <typename T>
-concept Integral = std::is_integral_v<T>; 
+concept Integral = std::is_integral<T>::value;
 
 template <Integral T>
-T mode(const T *values, std::size_t length)
-{
+T mode(const T *values, std::size_t length) {
+
 	if (length <= 0) return 0;
-	if (!values)     throw std::invalid_argument{ "Invalid argument passed." };
+	if (!values)     throw std::invalid_argument{ "mode(const T *values, std::size_t length): null pointer" };
 
 	T number{ values[0] };
 	T mode{ number };
 	int count{ 1 };
 	int count_mode{ 1 };
 
-	for (std::size_t i{ 1 }; i < length; ++i)
-	{
+	for (std::size_t i{ 1 }; i < length; ++i) {
+
 		if (values[i] == number)
 			++count;
-		else
-		{
-			if (count > count_mode)
-			{
+		else {
+
+			if (count > count_mode) {
+
 				count_mode = count;
 				mode = number;
 			}
@@ -48,26 +49,26 @@ T mode(const T *values, std::size_t length)
 	return mode;
 }
 
-int main()
-{
+int main() {
+
 	int int_array[]{ 2, 8, 1, 1, 2, 9, 1, 5, 5, 3 };
 	std::size_t length{ std::size(int_array) };
 
-	int float_array[]{2.0f, 8.0f, 1.0f, 1.0f, 2.0f, 9.0f, 1.0f, 5.0f, 5.0f, 3.0f};
+	float float_array[]{2.0f, 8.0f, 1.0f, 1.0f, 2.0f, 9.0f, 1.0f, 5.0f, 5.0f, 3.0f};
 
 	std::sort(std::begin(int_array), std::end(int_array));
 	std::sort(std::begin(float_array), std::end(float_array));
 
 	try {
 
-		auto works    = mode(int_array, length);
-	//	auto no_works = mode(float_array, length); <-- uncommenting this will cause an error
+		auto works = mode(int_array, length);
+	 // auto no_works = mode(float_array, length); <-- uncommenting this will cause an error
 
 		// If exception is thrown, this won't execute.
 		std::cout << "mode: " << works << '\n';
 		//std::cout << "mode: " << no_works << '\n';
 	}
-	catch (const std::exception &e) {
+	catch (const std::invalid_argument &e) {
 
 		std::cerr << "exception: " << e.what() << '\n';
 		return 1;
