82eb1735a55b20575c1326cec2059302fdf2cb8d
Gabriel <gabe.luculescu@gmail.com>
2021-01-15 13:21:28 -0600

    Initial commit

---
+++ Part 1/Chapter 7/7-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/*  Create an UnsignedBigInteger class that can handle numbers bigger than
+ *  a long. You can use a byte array as the internal representation (for example,
+ *  uint8_t[] or char[]). Implement operator overloads for operator+ and operator-.
+ *  Perform runtime checks for overflow. For the intrepid, also implement operator*,
+ *  operator/, and operator%. Make sure that your operator overloads work for
+ *  both int types and UnsignedBigInteger types. Implement an operator int type
+ *  conversion. Perform a runtime check if narrowing would occur
+ */
